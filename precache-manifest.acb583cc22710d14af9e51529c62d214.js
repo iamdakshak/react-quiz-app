3a2b8a12a64b88a34cd8a29a1b3941ffd339788d
Dakshak Nagrale <dakshak1997@rediffmail.com>
2020-09-24 01:35:37 +0530

    Updates

---
+++ precache-manifest.acb583cc22710d14af9e51529c62d214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a2bc851465c5b94a6a0737cfc34e63",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "f931fd85bc0811e9ab49",
+    "url": "/react-quiz-app/static/css/main.270bfac1.chunk.css"
+  },
+  {
+    "revision": "b6d92c1a46e78c9962bc",
+    "url": "/react-quiz-app/static/js/2.406eba10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.406eba10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f931fd85bc0811e9ab49",
+    "url": "/react-quiz-app/static/js/main.c7813da4.chunk.js"
+  },
+  {
+    "revision": "4cb17b25e4d85c130919",
+    "url": "/react-quiz-app/static/js/runtime-main.0678ee4d.js"
+  }
+]);
